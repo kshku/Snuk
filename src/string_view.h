f90accd6bf98ecea75fbfdd0cf072cbdbb46d7b2
kshku <1211shree@gmail.com>
2026-04-19 13:45:08 +0530

    feat(string_view): add SnukStringView

---
+++ src/string_view.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "defines.h"
+
+#include "snuk_string.h"
+#include "memory.h"
+#include <string.h>
+
+typedef struct SnukStringView {
+    const char *str;
+    uint64_t len;
+} SnukStringView;
+
+SNUK_INLINE SnukStringView snuk_string_view_create_with_len(const char *str, uint64_t len) {
+    return (SnukStringView){
+        .str = str,
+        .len = len,
+    };
+}
+
+SNUK_INLINE SnukStringView snuk_string_view_create(const char *str) {
+    return snuk_string_view_create_with_len(str, string_length(str));
+}
+
+SNUK_INLINE char *snuk_string_view_get_cstr(SnukStringView view) {
+    char *str = snuk_alloc((view.len + 1) * sizeof(char), alignof(char));
+    memcpy(str, view.str, view.len);
+    str[view.len] = 0;
+    return str;
+}
