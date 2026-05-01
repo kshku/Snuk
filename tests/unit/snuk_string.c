7184854af2d079bc1f1113417ad3ef1aeeb127b5
kshku <1211shree@gmail.com>
2026-05-01 14:33:42 +0530

    test(snuk_string): add few tests

--- tests/unit/snuk_string.c
+++ tests/unit/snuk_string.c
@@ -2,27 +2,42 @@
 
 #include <snuk_string.h>
 
+ADD_TEST(test_string_length) {
+    ASSERT_EQ(snuk_string_length("Hello"), 5);
+    ASSERT_EQ(snuk_string_length(""), 0);
+
+    TEST_PASSED;
+}
+
+ADD_TEST(test_string_equal) {
+    ASSERT(snuk_string_equal("Hi", "Hi"));
+    ASSERT(snuk_string_equal("", ""));
+    ASSERT(!snuk_string_equal("Hello", "Hi"));
+
+    TEST_PASSED;
+}
+
 ADD_TEST(test_string_concat) {
     const char *a = "Hello, ";
     const char *b = "World!";
 
     char *c = snuk_string_concat(a, 0, b, 0);
+    ASSERT(snuk_string_equal(c, "Hello, World!"));
     snuk_free(c);
-    log_trace("%s", c);
 
     c = snuk_string_concat(a, 3, b, 2);
-    log_trace("%s", c);
+    ASSERT(snuk_string_equal(c, "HelWo"));
     snuk_free(c);
 
     c = snuk_string_concat(a, 6, b, 6);
-    log_trace("%s", c);
+    ASSERT(snuk_string_equal(c, "Hello,World!"));
     snuk_free(c);
 
     c = snuk_string_concat(a, 7, b, 6);
-    log_trace("%s", c);
+    ASSERT(snuk_string_equal(c, "Hello, World!"));
     snuk_free(c);
 
-    return true;
+    TEST_PASSED;
 }
 
 RUN_ALL_TESTS();
