1672fff241c1a7d0e647cfbc5790d0b803ab0e25
kshku <1211shree@gmail.com>
2026-05-25 23:05:14 +0530

    test: count the ref counters by running docs/snuk_language.snuk
    Yeah! now you can run that entire file!

--- tests/count_ref_counters.sh
+++ tests/count_ref_counters.sh
@@ -21,3 +21,24 @@ do
     '
     echo -e "\n"
 done
+
+file=docs/snuk_language.snuk
+
+echo $file
+build/snuk "$file" \
+| grep -E '\[DEBUG\]: (created a ref counter|a ref counter got destroyed)' \
+| awk '
+BEGIN {
+    created = 0
+    destroyed = 0
+}
+{
+    if ($0 ~ /created/) created++;
+    else destroyed++;
+}
+END {
+    print "Created: " created
+    print "Destroyed: " destroyed
+}
+'
+echo -e "\n"
