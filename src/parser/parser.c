866ddbd7210e9024c7b537e577fb344617a818da
K Shreekrishna Upadhyaya <1211shree@gmail.com>
2026-05-30 19:07:54 +0530

    feat(parser): pass errors as item (#178)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,52 +1,46 @@
 #include "snuk/parser/parser.h"
 
-#include "snuk/io.h"
+#include "snuk/parser/parser_common.h"
 #include "snuk/parser/snuk_item.h"
 
-SnukItem *snuk_parser_next_item(SnukParser *parser) {
-    if (parser->current.type == SNUK_TOKEN_EOF) return NULL;
-    SnukItem *item = snuk_item_parse(parser);
-    if (parser->panic_mode) parser_sync(parser);
-    return item;
+void snuk_parser_init(SnukParser *parser, const char *src, SnukAllocator *allocator) {
+    *parser = (SnukParser){
+        .allocator = allocator,
+        .panic_mode = false,
+    };
+    snuk_lexer_init(&parser->lexer, src);
+
+    parser->previous = (SnukToken){0};
+    parser->current = snuk_lexer_next_token(&parser->lexer);
+    if (parser->current.type == SNUK_TOKEN_ERROR) parser_error(parser, "lexer error");
+    parser->next = snuk_lexer_next_token(&parser->lexer);
+}
+
+void snuk_parser_deinit(SnukParser *parser) {
+    if (!parser) return;
+    snuk_lexer_deinit(&parser->lexer);
+    *parser = (SnukParser){0};
 }
 
 void parser_error(SnukParser *parser, const char *err_msg) {
     if (parser->panic_mode) return;
 
     parser->panic_mode = true;
-    parser->had_error = true;
 
-    SnukToken t = parser->current;
-    snuk_eprint("%lu:%lu error", t.line, t.col);
-    if (t.type == SNUK_TOKEN_EOF) snuk_eprint(" at end");
-    else snuk_eprint(" at '" SNUK_STRING_VIEW_FORMAT "'", SNUK_STRING_VIEW_ARG(t.string_literal));
-    snuk_eprintln(" message: '%s'", err_msg);
+    parser->err_msg = err_msg;
+    parser->err_token = parser->current;
 }
 
-void parser_sync(SnukParser *parser) {
-    // TODO: skipping errors
+SnukItem *parser_sync(SnukParser *parser) {
     parser->panic_mode = false;
-    return;
+    if (parser->previous.type != SNUK_TOKEN_VSEMICOLON && parser->previous.type != SNUK_TOKEN_SEMICOLON)
+        while (!parser_match_item_end(parser)) parser_advance(parser);
+    return build_error_item(parser, parser->err_msg, parser->err_token);
+}
 
-    while (parser->current.type != SNUK_TOKEN_EOF) {
-        if (parser->previous.type == SNUK_TOKEN_SEMICOLON) return;
-
-        switch (parser->current.type) {
-            case SNUK_TOKEN_IF:
-            case SNUK_TOKEN_WHILE:
-            case SNUK_TOKEN_FOR:
-            case SNUK_TOKEN_DO:
-            case SNUK_TOKEN_RETURN:
-            case SNUK_TOKEN_VAR:
-            case SNUK_TOKEN_CONST:
-            case SNUK_TOKEN_LBRACE:
-            case SNUK_TOKEN_LINE_COMMENT:
-            case SNUK_TOKEN_BLOCK_COMMENT:
-                return;
-            default:
-                break;
-        }
-
-        parser_advance(parser);
-    }
+SnukItem *snuk_parser_next_item(SnukParser *parser) {
+    if (parser->current.type == SNUK_TOKEN_EOF) return NULL;
+    SnukItem *item = snuk_item_parse(parser);
+    if (parser->panic_mode) return parser_sync(parser);
+    return item;
 }
