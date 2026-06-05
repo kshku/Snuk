6f6a7a3cc58ddc638d4d56f50910939a9598ace0
K Shreekrishna Upadhyaya <1211shree@gmail.com>
2026-06-05 11:21:23 +0530

    feat(interpreter): impelemnt const declarations (#200)
    * feat(interpreter): impelemnt const declarations
    
    * test: const test

--- include/snuk/interpreter/snuk_env.h
+++ include/snuk/interpreter/snuk_env.h
@@ -15,24 +15,28 @@ struct SnukEnv {
     SnukStringView name;
     SnukType *type;
     SnukValue value;
+    bool is_const;
 };
 
 /**
  * @brief Allocate a SnukEnv and populate it by evaluating the given expression.
  */
-SNUK_INLINE SnukEnv *snuk_env_create(SnukStringView name, SnukType *type, SnukValue value) {
+SNUK_INLINE SnukEnv *snuk_env_create(SnukStringView name, SnukType *type, SnukValue value, bool is_const) {
     SnukEnv *env = (SnukEnv *)snuk_alloc(sizeof(SnukEnv), alignof(SnukEnv));
     *env = (SnukEnv){
         .name = name,
         .type = type,
         .value = snuk_value_copy(value),
+        .is_const = is_const,
     };
     return env;
 }
 
-SNUK_INLINE void snuk_env_assign_value(SnukEnv *env, SnukValue value) {
+SNUK_INLINE bool snuk_env_assign_value(SnukEnv *env, SnukValue value) {
+    if (env->is_const) return false;
     snuk_value_free(env->value);
     env->value = snuk_value_copy(value);
+    return true;
 }
 
 SNUK_INLINE void snuk_env_free(SnukEnv *env) {
